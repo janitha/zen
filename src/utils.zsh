0c68663381ce61bdb4e1bf79326e2f11a8f048da
Janitha Karunaratne <janitha@janitha.com>
2024-07-10 15:19:54 -0500

    Added utils zsh

---
+++ src/utils.zsh
@@ -0,0 +1,5 @@
+OSTYPE=$(uname -s)
+alias is_linux='[[ $OSTYPE == "Linux" ]]'
+alias is_darwin='[[ $OSTYPE == "Darwin" ]]'
+alias is_macos='[[ $OSTYPE == "Darwin" ]]'
+alias is_freebsd='[[ $OSTYPE == "FreeBSD" ]]'
